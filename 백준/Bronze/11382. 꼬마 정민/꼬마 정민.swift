de1128658ad8049cc941d426743c45b284f569e1
oww10 <dntjr2209@gmail.com>
2025-08-26 19:11:53 +0900

    [Bronze V] Title: 꼬마 정민, Time: 4 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.swift
@@ -0,0 +1,7 @@
+    if let input = readLine(){
+        
+        let numbers = input.split(separator: " ").map{Int($0)!}
+        
+        print (numbers[0] + numbers[1] + numbers [2])
+        
+    }

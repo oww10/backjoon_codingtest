2eb4357ab7c767696b0e586e9d325401c5ecbb83
oww10 <dntjr2209@gmail.com>
2025-09-03 19:38:21 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.swift
@@ -0,0 +1,12 @@
+let nBites = Int(readLine()!)
+
+let longNum = nBites! / 4
+var longText = ""
+
+for _ in 0..<longNum{
+    let longStr = "long "
+    
+    longText = longText + longStr
+}
+
+print("\(longText)int ")

332c44279a406776aaf83d52ca5524bff8f4e94a
oww10 <dntjr2209@gmail.com>
2025-09-24 19:52:13 +0900

    [Bronze II] Title: 다이얼, Time: 8 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.swift
@@ -0,0 +1,38 @@
+
+import Foundation
+
+func dial() {
+    if let input = readLine() {
+        var totalTime = 0
+        for char in input {
+            totalTime += getTime(for: char)
+        }
+        
+        print(totalTime)
+    }
+}
+
+func getTime(for character: Character) -> Int{
+    switch character{
+    case "A","B","C":
+        return 3
+    case "D","E","F":
+        return 4
+    case "G","H","I":
+        return 5
+    case "J","K","L":
+        return 6
+    case "M","N","O":
+        return 7
+    case "P","Q","R","S":
+        return 8
+    case "T","U","V":
+        return 9
+    case "W","X","Y","Z":
+        return 10
+    default:
+        return 0
+    }
+}
+
+dial()
